be34934ecc988c091febd18133c8e43a46fe3314
MAYC8314 <kojiedgarmiyazaki@gmail.com>
2025-02-17 16:34:20 -0500

    Milestone 1: Initial Backend & Frontend Setup

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const lessonsRouter = require('./routes/lessons');
-app.use('/lessons', lessonsRouter);
-
 // Define a simple test route
 app.get('/', (req, res) => {
   res.send("Hello, TalentLMS!");
