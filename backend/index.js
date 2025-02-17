dabf49707c4104b1b5127d529201563409231e13
MAYC8314 <kojiedgarmiyazaki@gmail.com>
2025-02-17 16:18:29 -0500

    Milestone 1: Initial Backend & Frontend Setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Define a simple test route
+app.get('/', (req, res) => {
+  res.send("Hello, TalentLMS!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
